7491824a4a3b4caee977b2cb3281992ee5b0fe09
kean liu <kean_liu@hotmail.com>
2024-06-29 19:43:15 +0800

    update

--- main.py
+++ main.py
@@ -112,13 +112,15 @@ def check_available_court():
         for court, time_info in result:
             msg_text = msg_text + court[1][1] + time_info['v'] + '/r/n'
         logging.info(msg_text)
-    to_user = '@e0bfaae9860606cacb2db54737953ee34dd6c66329014a9ec63a3b7384c69a20' # 又胖又能吃群
-    # to_user = "@@44c1b8fd4cb16cea6836384ca89fbacae12d60cd9dffd842d8f86cd4996f3362" # 奥体羽毛球群
-    itchat.send_msg(to_user,  msg_text)
+        to_user = '@e0bfaae9860606cacb2db54737953ee34dd6c66329014a9ec63a3b7384c69a20' # 又胖又能吃群
+        to_user = "@@44c1b8fd4cb16cea6836384ca89fbacae12d60cd9dffd842d8f86cd4996f3362" # 奥体羽毛球群
+        msg_text = '牛气！'
+        itchat.send_msg(to_user,  msg_text)
 
-scheduler = BackgroundScheduler({'apscheduler.job_defaults.max_instances': 300})
-# scheduler = BackgroundScheduler()
+# scheduler = BackgroundScheduler({'apscheduler.job_defaults.max_instances': 300})
+scheduler = BackgroundScheduler()
 scheduler.remove_all_jobs() 
+# scheduler.add_job(runsync, 'interval', seconds=1, max_instances=2)
 scheduler.add_job(check_available_court, 'interval', seconds=600)
 
 try:
